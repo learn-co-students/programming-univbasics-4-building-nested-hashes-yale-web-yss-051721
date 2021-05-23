112138f563c3fb47b9616b3871c6a4d7cd88a5ff
Bill Feng <bill.feng@yale.edu>
2021-05-23 06:57:53 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+  {railroads: {}}  
 
 end
 
@@ -9,7 +10,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  basehash=base_hash
+  basehash[:railroads][:pieces]=4
+  basehash[:railroads][:rent_in_dollars]={}
+  basehash[:railroads][:names]={}
+  basehash
 end
 
 def monopoly_with_third_tier
@@ -17,7 +22,16 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  secondtierhash=monopoly_with_second_tier
+  secondtierhash[:railroads][:rent_in_dollars][:one_piece_owned]=25;
+  secondtierhash[:railroads][:rent_in_dollars][:two_pieces_owned]=50;
+    secondtierhash[:railroads][:rent_in_dollars][:three_pieces_owned]=100;
+        secondtierhash[:railroads][:rent_in_dollars][:four_pieces_owned]=200;
+  secondtierhash[:railroads][:names][:reading_railroad]={}
+    secondtierhash[:railroads][:names][:pennsylvania_railroad]={}
+      secondtierhash[:railroads][:names][:b_and_o_railroad]={}
+        secondtierhash[:railroads][:names][:shortline_railroad]={}
+  secondtierhash
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +39,12 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  thirdtierhash=monopoly_with_third_tier
+  thirdtierhash[:railroads][:names][:reading_railroad][:mortgage_value]=100
+    thirdtierhash[:railroads][:names][:pennsylvania_railroad][:mortgage_value]=200
+    thirdtierhash[:railroads][:names][:b_and_o_railroad][:mortgage_value]=400
+        thirdtierhash[:railroads][:names][:shortline_railroad][:mortgage_value]=800
 
+
+  thirdtierhash
 end
