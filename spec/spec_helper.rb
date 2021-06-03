aeb6236037800885d20236d67ce202e3c59a169d
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-06-03 15:48:37 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,22 @@ require 'yaml'
 
 RSpec.configure do |config|
 
-  def __
-    # raise "replace underscore with code"
+  def base_hash
+ # return a valid hash with any key/value pair of your choice
+ 
+  
+  end
+  
+  def monopoly_with_second_tier
+    
+  end
+  
+  def monopoly_with_third_tier
+    
+  end
+  
+  def monopoly_with_fourth_tier
+    
   end
 
 end
